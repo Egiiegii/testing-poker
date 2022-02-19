c339159345b84c887ba3f648b10bb3f86a8e5916
Van Riko <erdenebulag.0812@gmail.com>
2022-02-19 15:10:15 +0900

    added one pair

--- com/egiiegii/poker/core/hand.py
+++ com/egiiegii/poker/core/hand.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from collections import Counter
 from com.egiiegii.poker.core.dataclasses import Card
 
 HAND_MAX = 5
@@ -15,7 +15,20 @@ class Hand:
             raise RuntimeError("Card over 5")
 
     def find_score(self):
-        return max(self.cards)
+        # 2oos deesh ijil modnudig bugdin gargana
+        # ter dundasa unduriig ni olno
+        # undur modondeere one pair iin score iig nemne. +13
+        counter = Counter(self.cards)
+        pairs = []
+        for (key, value) in counter.most_common():
+            if value > 1:
+                pairs.append(key)
+
+        if len(pairs) > 0:
+            score = max(pairs) + 13
+        else:
+            score = max(self.cards)
+        return score
 
     def get_card_name(self):
         return [card.name for card in self.cards]
--- test/com/egiiegii/poker/logic/poker_game.py
+++ test/com/egiiegii/poker/logic/poker_game.py
@@ -23,6 +23,87 @@ class PokerGameTest(unittest.TestCase):
         reality = poker_game.winner()
         expect_len = 1
         expect = 0
-        print(reality)
         assert len(reality) == expect_len
         assert reality[0] == expect
+
+    def test_high_card_tie(self):
+        pick_card_mock = MagicMock()
+        pick_card_mock.side_effect = [
+            Card.ACE, Card.ACE,
+            Card.JACK, Card.QUEEN,
+            Card.TEN, Card.NINE,
+            Card.TWO, Card.TWO,
+            Card.THREE, Card.THREE
+        ]
+
+        poker_game = PokerGame()
+        poker_game.pick_card = pick_card_mock
+        poker_game.shuffle()
+        poker_game.print()
+        reality = poker_game.winner()
+        expect_len = 2
+        expects = [0, 1]
+        assert len(reality) == expect_len
+        for expect in expects:
+            assert expect in reality
+
+    def test_high_card_one_pair_win(self):
+        pick_card_mock = MagicMock()
+        pick_card_mock.side_effect = [
+            Card.ACE, Card.JACK,
+            Card.JACK, Card.QUEEN,
+            Card.TEN, Card.NINE,
+            Card.TWO, Card.TWO,
+            Card.THREE, Card.TWO
+        ]
+
+        poker_game = PokerGame()
+        poker_game.pick_card = pick_card_mock
+        poker_game.shuffle()
+        poker_game.print()
+        reality = poker_game.winner()
+        expect_len = 1
+        expect = 1
+        assert len(reality) == expect_len
+        assert reality[0] == expect
+
+    def test_high_card_one_pair_win_2(self):
+        pick_card_mock = MagicMock()
+        pick_card_mock.side_effect = [
+            Card.ACE, Card.JACK,
+            Card.JACK, Card.QUEEN,
+            Card.TEN, Card.NINE,
+            Card.TEN, Card.TWO,
+            Card.THREE, Card.TWO
+        ]
+
+        poker_game = PokerGame()
+        poker_game.pick_card = pick_card_mock
+        poker_game.shuffle()
+        poker_game.print()
+        reality = poker_game.winner()
+        expect_len = 1
+        expect = 0
+        assert len(reality) == expect_len
+        assert reality[0] == expect
+
+    def test_high_card_one_pair_tie(self):
+        pick_card_mock = MagicMock()
+        pick_card_mock.side_effect = [
+            Card.ACE, Card.JACK,
+            Card.JACK, Card.QUEEN,
+            Card.TEN, Card.TEN,
+            Card.TEN, Card.TEN,
+            Card.THREE, Card.TWO
+        ]
+
+        poker_game = PokerGame()
+        poker_game.pick_card = pick_card_mock
+        poker_game.shuffle()
+        poker_game.print()
+        reality = poker_game.winner()
+        expect_len = 2
+        expects = [0, 1]
+        assert len(reality) == expect_len
+        for expect in expects:
+            assert expect in reality
